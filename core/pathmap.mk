fb1dd2ccbbbbdd27710b7cfe43fac7373526a9bb
Simon Shields <simon@lineageos.org>
2018-03-02 12:55:47 +1100

    build: unconditionally use PixysOS pathmap
    even if we're not building a lineage target, we need to
    use the PixysOS pathmap to make things like recovery and ril
    happy
    
    Change-Id: I974c30ad10d4ff5b7805c3df9c22010f1e002bf1

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -93,6 +93,4 @@ FRAMEWORKS_BASE_SUBDIRS := \
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
 
-ifneq ($(PIXYS_BUILD),)
 -include $(TOPDIR)vendor/pixys/build/core/pathmap.mk
-endif
