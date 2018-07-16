e0b65161a79eda0809ea244185d7d1499adda868
ElDainosor <eldainosor@gmail.com>
2018-07-16 12:57:35 +0000

    build: Use old breakfast method
    This makes the brunch method works as intended

--- envsetup.sh
+++ envsetup.sh
@@ -589,12 +589,20 @@ function brunch()
     return $?
 }
 
+function omnom
+{
+    brunch $*
+    eat
+}
+
 function breakfast()
 {
     target=$1
-    CUSTOM_DEVICES_ONLY="true"
+    local variant=$2
+    PIXYS_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
-    for f in `/bin/ls vendor/noobbuilds/vendorsetup.sh 2> /dev/null`
+    add_lunch_combo full-eng
+    for f in `/bin/ls vendor/pixys/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
@@ -610,8 +618,11 @@ function breakfast()
             # A buildtype was specified, assume a full device name
             lunch $target
         else
-            # This is probably just the nexus model name
-            lunch $target-user
+            # This is probably just the PixysOS model name
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch bootleg_$target-$variant
         fi
     fi
     return $?
