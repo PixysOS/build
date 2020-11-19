9750971c73176071e799655d8ad7e0123865bd85
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-19 17:16:07 +0300

    product: Exclude AOSP sounds on Pixys build
    Change-Id: I4a221bc5341d8825819d1e435d2ee629e9cf7458

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -28,9 +28,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
 
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
 
+ifeq ($(PIXYS_BUILD),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.com.android.dataroaming=true \
+    ro.com.android.dataroaming=true
+endif
 
 PRODUCT_PACKAGES += \
     PhotoTable \
